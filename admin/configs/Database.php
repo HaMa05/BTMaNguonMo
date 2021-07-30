4b4dd1a7f596acd8230c0b4428bccc3c7e6606ce
HaMa05 <vanha.jungle@gmail.com>
2021-07-30 12:16:56 +0700

    change config db

--- admin/configs/Database.php
+++ admin/configs/Database.php
@@ -1,6 +1,6 @@
 <?php
 class Database {
-    const DB_DSN = 'mysql:host=localhost;dbname=lp_shop;charset=utf8';
+    const DB_DSN = 'mysql:host=localhost;dbname=nhom15;charset=utf8';
     const DB_USERNAME = 'root';
     const DB_PASSWORD = '';
 }
